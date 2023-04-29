01fe20588217cd5b06d3052142c5d0e3f1be9298
Mateusz Bratkowski <mateusz.bratkowski93@gmail.com>
2023-04-29 19:33:45 +0200

    Apply handling a nullable types from API response

--- core-data-network/src/main/java/io/bratexsoft/core/network/datasource/RepositoriesNetworkDataSourceImpl.kt
+++ core-data-network/src/main/java/io/bratexsoft/core/network/datasource/RepositoriesNetworkDataSourceImpl.kt
@@ -2,30 +2,35 @@ package io.bratexsoft.core.network.datasource
 
 import io.bratexsoft.core.data.api.error.RepositoryNotFoundException
 import io.bratexsoft.core.data.api.model.CommitsListItem
+import io.bratexsoft.core.data.api.model.Id
+import io.bratexsoft.core.data.api.model.Name
 import io.bratexsoft.core.data.api.model.OrganizationInformation
 import io.bratexsoft.core.data.api.model.Repositories
 import io.bratexsoft.core.network.api.RepositoriesServiceApi
+import io.bratexsoft.core.network.mapper.RepositoriesMapper
 import io.bratexsoft.core.network.model.commits.toDomain
 import io.bratexsoft.core.network.model.repositories.toDomain
 import retrofit2.HttpException
 
 class RepositoriesNetworkDataSourceImpl(
-    private val repositoriesServiceApi: RepositoriesServiceApi
+    private val repositoriesServiceApi: RepositoriesServiceApi,
+    private val repositoriesMapper: RepositoriesMapper,
 ) : RepositoriesNetworkDataSource {
 
     @SuppressWarnings("TooGenericExceptionCaught")
     override suspend fun getRepositories(organizationInformation: OrganizationInformation): Repositories {
         try {
             return repositoriesServiceApi.getRepositories(
-                organizationInformation.ownerName,
-                organizationInformation.repositoryName
-            ).toDomain()
+                ownerName = organizationInformation.ownerName,
+                repositoryName = organizationInformation.repositoryName
+            ).let { repository -> repositoriesMapper.mapTo(repository) }
         } catch (e: Throwable) {
             when (e) {
                 // I didn't have a time to make a correct error handling
                 is HttpException -> {
                     throw RepositoryNotFoundException()
                 }
+
                 else -> throw e
             }
         }
@@ -33,8 +38,8 @@ class RepositoriesNetworkDataSourceImpl(
 
     override suspend fun getRepositoryCommits(organizationInformation: OrganizationInformation): List<CommitsListItem> {
         return repositoriesServiceApi.getRepositoryCommits(
-            organizationInformation.ownerName,
-            organizationInformation.repositoryName
+            ownerName = organizationInformation.ownerName,
+            repositoryName = organizationInformation.repositoryName
         ).map { it.toDomain() }
     }
 }
---
+++ core-data-network/src/test/java/io/bratexsoft/core/network/RepositoryMapperTest.kt
@@ -0,0 +1,79 @@
+package io.bratexsoft.core.network
+
+import io.bratexsoft.core.network.mapper.RepositoriesMapper
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import io.bratexsoft.core.data.api.model.Repositories
+import io.bratexsoft.core.network.model.repositories.RepositoriesResponseApi
+import org.junit.Before
+import java.util.UUID
+import kotlin.random.Random
+
+
+class RepositoryMapperTest {
+
+    private lateinit var repositoriesMapper: RepositoriesMapper
+
+    @Before
+    fun setup() {
+        repositoriesMapper = RepositoriesMapper()
+    }
+
+    @Test
+    fun `create a repository mapper with success`() {
+        assertThat(repositoriesMapper).isNotNull()
+        assertThat(repositoriesMapper).isInstanceOf(RepositoriesMapper::class.java)
+    }
+
+    @Test
+    fun `method mapTo returns repository object`() {
+        val repositoriesResponseApi = RepositoriesResponseApi(
+            id = randomInt(),
+            name = randomString()
+        )
+        val repository = repositoriesMapper.mapTo(repositoriesResponseApi = repositoriesResponseApi)
+
+        assertThat(repository).isNotNull()
+        assertThat(repository).isInstanceOf(Repositories::class.java)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `method mapTo throw exception when id is null`() {
+        val repositoriesResponseApi = RepositoriesResponseApi(
+            id = null,
+            name = randomString()
+        )
+
+        repositoriesMapper.mapTo(repositoriesResponseApi)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `method mapTo throw exception when name is null`() {
+        val repositoriesResponseApi = RepositoriesResponseApi(
+            id = randomInt(),
+            name = null,
+        )
+        repositoriesMapper.mapTo(repositoriesResponseApi)
+    }
+
+    @Test
+    fun `method mapTo returns repository object with parsed data from response`() {
+        val repositoryResponseApi = RepositoriesResponseApi(
+            id = randomInt(),
+            name = randomString()
+        )
+
+        val repository = repositoriesMapper.mapTo(repositoryResponseApi)
+
+        assertThat(repository.id.value).isEqualTo(repositoryResponseApi.id)
+        assertThat(repository.name.value).isEqualTo(repositoryResponseApi.name)
+    }
+}
+
+fun randomInt(): Int {
+    return Random.nextInt()
+}
+
+fun randomString(): String {
+    return UUID.randomUUID().toString()
+}
