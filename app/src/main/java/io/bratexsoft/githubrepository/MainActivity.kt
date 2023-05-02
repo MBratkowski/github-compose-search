c0f51d32d8a8221418cb9b53a27257bfadaf0642
Mateusz Bratkowski <mateusz.bratkowski93@gmail.com>
2023-05-02 17:23:19 +0200

    Making a test

--- app/src/main/java/io/bratexsoft/githubrepository/MainActivity.kt
+++ app/src/main/java/io/bratexsoft/githubrepository/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.bratexsoft.githubrepository
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate")
         setContent {
             GithubRepositoryTheme {
                 Surface(
